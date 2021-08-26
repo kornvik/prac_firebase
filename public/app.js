0cdacffb0477b213f1df3900a66ebcfc15b965cd
korn <kornvik.t@hotmail.com>
2021-08-26 21:28:55 +0900

    add login btn

---
+++ public/app.js
@@ -0,0 +1,41 @@
+///// User Authentication /////
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => {
+    console.log("sfdf");
+    auth.signInWithPopup(provider);
+};
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if(user){ //signin
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+
+    }
+})
+
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingList = document.getElementById('thingList');
